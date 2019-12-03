b0408bc834516cd25c5899c94d34144d163de425
Jo <jo_serendipity@icloud.com>
2019-12-03 15:32:54 +0000

    add ForecastDetails component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
 import ForecastSummaries from './forecast-summaries';
+import ForecastDetails from './forecast-details';
 import '../styles/app.css';
 import '../styles/forecast-summaries.css';
 
@@ -9,6 +10,7 @@ const App = props => (
   <div className="forecast">
     <LocationDetails city={props.location.city} country={props.location.country} />
     <ForecastSummaries forecasts={props.forecasts} />
+    <ForecastDetails />
   </div>
 );
 
--- src/components/forecast-details.jsx
+++ src/components/forecast-details.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
-import moment from 'moment';
 import PropTypes from 'prop-types';
 
-const ForecastDetails = props => {
-  return (
-    <div className="forecasts-details">
-      <h4 className="date">{moment(props.forecasts.date).format(ddd Do MMM)}</h4>
-      <p>Min Temp:</p>
-      <span className="tempMin" className="temp">
-        {props.forecasts.temperature.min}
-      </span>
-      <p>Max Temp:</p>
-      <span className="tempMax" className="temp">
-        {props.forecasts.temperature.max}
-      </span>
-      <p>Wind Speed:</p>
-      <span className="windSpeed">{props.forecasts.wind.speed}mph</span>
-      <p>Wind Direction:</p>
-  <span className="windDir">{props.forecasts.wind.direction}</span>
-    </div>;
-  )
-
+function ForecastDetails(props) {
+  return <span>{props.forecast}</span>;
 }
 
-
 ForecastDetails.propTypes = {
-  forecasts: PropTypes.shape({
-    date: PropTypes.numnber,
-    temperature: PropTypes.object,
-    wind: PropTypes.object,
-  }).isRequired,
+  forecast: PropTypes.array.isRequired,
 };
 
 export default ForecastDetails;
